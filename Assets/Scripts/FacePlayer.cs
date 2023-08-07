41aefe9e4d6405d302be0e4cb9b669113e5fda4a
yeokewei <yeokewei@gmail.com>
2023-08-07 21:48:25 +0200

    Wall colliders working with player
    WIP: Healthbar still not showing in PICO

--- Assets/Scripts/FacePlayer.cs
+++ Assets/Scripts/FacePlayer.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FacePlayer : MonoBehaviour
 {
+    public bool isEnemy;
     private Vector3 playerPos;
     private Vector3 npcPos;
 
-    void Update()
+    void Start()
     {
         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
         npcPos = gameObject.transform.position;
         Vector3 delta = new Vector3(playerPos.x - npcPos.x, 0.0f, playerPos.z - npcPos.z);
         Quaternion rotation = Quaternion.LookRotation(delta);
         gameObject.transform.rotation = rotation;
+    }
+    void Update()
+    {
+        if (isEnemy)
+        {
+            playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+            npcPos = gameObject.transform.position;
+            Vector3 delta = new Vector3(playerPos.x - npcPos.x, 0.0f, playerPos.z - npcPos.z);
+            Quaternion rotation = Quaternion.LookRotation(delta);
+            gameObject.transform.rotation = rotation;
+        }
     }
 }
